1677d1808423cc69fae67ca3f0cc9a55e3c909da
Jacob Marshall <jacob+github@manage.net.nz>
2015-04-04 11:40:09 +1300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+var REDIS_PORT = process.env.REDIS_PORT,
+    REDIS_HOST = process.env.REDIS_HOST,
+    WEB_PORT = process.env.PORT,
+    TASKS_KEY = 'tasks';
+
+var redis = require('redis'),
+    queue = redis.createClient(REDIS_PORT, REDIS_HOST),
+    express = require('express'),
+    parser = require('body-parser'),
+    multer = require('multer'),
+    app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(multer());
+
+app.use(express.static(__dirname + '/../public'));
+
+app.put('/tasks', function (req, res) {
+    console.log(req.body);
+});
+
+app.get('/tasks', function (req, res) {
+    queue.lrange(TASKS_KEY, function (err, data) {
+        console.log(err, data);
+    });
+});
+
+app.listen(WEB_PORT || 8000);
