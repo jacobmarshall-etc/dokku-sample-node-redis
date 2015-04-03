de633a92dccd1a812bb01a1d45c1b41d1aae1f96
Jacob Marshall <jacob+github@manage.net.nz>
2015-04-04 12:45:16 +1300

    Lets see if this pile o' crap works, shall we?

--- src/app.js
+++ src/app.js
@@ -11,6 +11,38 @@ var redis = require('redis'),
     uuid = require('node-uuid').v4,
     app = express();
 
+function tasks(callback) {
+    queue.lrange(TASKS_KEY, 0, -1, function (err, result) {
+        callback(err, result.map(function (task) {
+            return JSON.parse(task);
+        }));
+    });
+}
+
+function create(task, callback) {
+    var data = {
+        id: uuid(),
+        task: task,
+        complete: false
+    };
+
+    queue.rpush(TASKS_KEY, JSON.stringify(data), function (err, result) {
+        callback(err, data);
+    });
+}
+
+function remove(id, callback) {
+    tasks(function (data) {
+        data.forEach(function (task) {
+            if (task.id === id) {
+                queue.lrem(TASKS_KEY, 0, JSON.stringify(task), function (err, result) {
+                    callback(err);
+                });
+            }
+        });
+    });
+}
+
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(multer());
@@ -18,24 +50,20 @@ app.use(multer());
 app.use(express.static(__dirname + '/../public'));
 
 app.put('/tasks', function (req, res) {
-    var task = {
-        id: uuid(),
-        task: req.body.task,
-        complete: false
-    };
-
-    queue.rpush(TASKS_KEY, JSON.stringify(task), function (err, result) {
-        if (err) return res.send();
-
-        res.send(task);
+    create(req.body.task, function (err, task) {
+        res.send(err ? { error: err } : task);
     });
 });
 
 app.get('/tasks', function (req, res) {
-    queue.lrange(TASKS_KEY, 0, -1, function (err, data) {
-        res.send(data.map(function (task) {
-            return JSON.parse(task);
-        }));
+    tasks(function (data) {
+        res.send(data);
+    });
+});
+
+app.delete('/task/:id', function (req, res) {
+    remove(req.params.id, function (err) {
+        res.send(err ? { error: err } : { result: 'success' });
     });
 });
 
